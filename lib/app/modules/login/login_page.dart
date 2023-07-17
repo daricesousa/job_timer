151816966e04153419fdb79079f01d8c62537203
Darice Sousa <daricesousa@gmail.com>
2023-07-17 11:59:06 -0300

    implement layout login page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/app/extensions/colors_ext.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            context.primaryColorLight,
+            context.primaryColor,
+          ]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(height: screenSize.height * 0.1),
+              SizedBox(
+                width: screenSize.width * 0.9,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[200]),
+                  child: Image.asset('assets/images/google.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
