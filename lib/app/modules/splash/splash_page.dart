b2c60a9bb2117e64e517462a1425a91d99611c59
Darice Sousa <daricesousa@gmail.com>
2023-07-17 11:38:29 -0300

    add interceptor firebase-auth

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,8 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/extensions/colors_ext.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        Modular.to.navigate('/login');
+      } else {
+        Modular.to.navigate('/home');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
