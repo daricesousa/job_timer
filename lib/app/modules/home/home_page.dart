8fc582fd2833c888e95c447bd77da361e0e32994
Darice Sousa <daricesousa@gmail.com>
2023-09-23 15:41:00 -0300

    Update projects after creating a new project

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -40,7 +40,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               SliverPersistentHeader(
-                delegate: HeaderProjectsMenu(),
+                delegate: HeaderProjectsMenu(controller: controller),
                 pinned: true,
               ),
               BlocSelector<HomeController, HomeState, bool>(
--- lib/app/modules/home/widgets/header_projects_menu.dart
+++ lib/app/modules/home/widgets/header_projects_menu.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
 import 'package:job_timer/app/entities/projects_status_enum.dart';
+import 'package:job_timer/app/modules/home/controller/home_controller.dart';
 
 class HeaderProjectsMenu extends SliverPersistentHeaderDelegate {
+  final HomeController controller;
+
+  HeaderProjectsMenu({
+    required this.controller,
+  });
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
@@ -36,8 +43,9 @@ class HeaderProjectsMenu extends SliverPersistentHeaderDelegate {
             SizedBox(
               width: constraints.maxWidth * 0.4,
               child: ElevatedButton.icon(
-                onPressed: () {
-                  Modular.to.pushNamed('/project/register/');
+                onPressed: () async {
+                  await Modular.to.pushNamed('/project/register/');
+                  controller.loadProjects();
                 },
                 icon: const Icon(Icons.add),
                 label: const Text("Novo Projeto"),
